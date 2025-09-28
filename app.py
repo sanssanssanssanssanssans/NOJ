e17fe9b58e6f9d152e27f91188b81e656ea24129
sanssanssanssanssanssans <ajona5427@kakao.com>
2025-09-28 17:58:32 +0900

    Update app.py

--- app.py
+++ app.py
@@ -177,5 +177,6 @@ def submit(pid):
     return redirect('/status')
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8000)
-
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
